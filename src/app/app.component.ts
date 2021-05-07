5cb2ef3ba1fcb56ec0b35c6606aec95b5d4987c9
Nitish <jainnitish817@gmail.com>
2021-05-07 16:52:46 +0530

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,25 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+
+export class AppComponent {
+
+  /*
+  names:string[]=[];
+  addName(name:string){
+    this.names.push(name);
+   // console.log(name)
+  }
+
+  status:boolean = false;
+  changeStatus(){
+    this.status = !this.status
+  }
+  */
+
+  
+}
---
+++ src/app/product/product.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-product',
+  templateUrl: './product.component.html',
+  styleUrls: ['./product.component.css']
+})
+export class ProductComponent{
+
+  products=[1,2,3,4,5,6,7,8];
+
+}
